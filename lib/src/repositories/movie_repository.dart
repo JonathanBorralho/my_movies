897bb5245fcb7630cba6dc488cd0f0bba4529a18
JonathanBorralho <jonathan.borralho@gmail.com>
2019-07-23 17:32:31 -0300

    Localização dos filmes para pt-BR

--- lib/src/repositories/movie_repository.dart
+++ lib/src/repositories/movie_repository.dart
@@ -8,7 +8,7 @@ class MovieRepository {
   final String _apiKey = '5ea66116adf9299cc57dc1eacc39f2c6';
 
   Future<List<Movie>> findAll() async {
-    final url = 'https://api.themoviedb.org/3/movie/popular?api_key=$_apiKey';
+    final url = 'https://api.themoviedb.org/3/movie/popular?api_key=$_apiKey&language=pt-BR';
     http.Response response = await http.get(url);
     Map<String, dynamic> body = convert.jsonDecode(response.body);
     MovieResponsePage page = MovieResponsePage.fromJson(body);
